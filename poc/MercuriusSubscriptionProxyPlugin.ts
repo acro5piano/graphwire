f61851181638049b33a9c792a8ba1174f5be0a37
gosho-kazuya <ketsume0211@gmail.com>
2021-08-11 23:00:19 +0900

    create mercurius plugin

---
+++ poc/MercuriusSubscriptionProxyPlugin.ts
@@ -0,0 +1,100 @@
+import { FastifyPluginAsync } from 'fastify'
+import mercurius, { IResolvers } from 'mercurius'
+import { introspectSchema, wrapSchema } from '@graphql-tools/wrap'
+import { print, printSchema } from 'graphql'
+import { Static, Type } from '@sinclair/typebox'
+import got from 'got'
+import { MQEmitterRedis } from 'mqemitter-redis'
+
+import { gql } from './util'
+
+const fakeSchema = gql`
+  type Query {
+    ok: String
+  }
+`
+
+interface MercuriusSubscriptionProxyPluginOptions {
+  emitter: MQEmitterRedis
+  upstreamUrl: string
+}
+
+export const MercuriusSubscriptionProxyPlugin: FastifyPluginAsync<MercuriusSubscriptionProxyPluginOptions> =
+  async (app, options) => {
+    const executor = async ({ document, variables }: any) => {
+      const query = print(document)
+      const fetchResult = await got
+        .post(options.upstreamUrl, {
+          json: { query, variables },
+        })
+        .json()
+      return fetchResult as any
+    }
+
+    const rawSchema = wrapSchema({
+      schema: await introspectSchema(executor),
+      executor,
+    })
+
+    const remoteSchemaWithSubscription = printSchema(rawSchema).replace(
+      'type Query',
+      'type Subscription',
+    )
+
+    const schema = `${remoteSchemaWithSubscription}\n\n${fakeSchema}`
+
+    const resolvers: IResolvers = {
+      Query: {
+        ok: () => 'ok',
+      },
+      Subscription: {
+        user: {
+          subscribe: async (...params) => {
+            const [, , { pubsub }] = params
+            process.nextTick(() => {
+              pubsub.publish({ topic: `user:1`, payload: {} })
+            })
+            return pubsub.subscribe(`user:1`)
+          },
+          async resolve(...params) {
+            const [, , , info] = params
+            const query = print(info.operation).replace('subscription', 'query')
+            const { data } = await got
+              .post(options.upstreamUrl, {
+                json: {
+                  query,
+                },
+              })
+              .json()
+            return data.user
+          },
+        },
+      },
+    }
+
+    const invalidateSchema = Type.Object({
+      topic: Type.String(),
+    })
+
+    app.post<{ Body: Static<typeof invalidateSchema> }>(
+      '/v1/publish',
+      { schema: { body: invalidateSchema } },
+      (request, reply) => {
+        const { topic } = request.body
+        options.emitter.emit({ topic }, (err) => {
+          if (err) {
+            reply.status(500).send(err)
+          }
+          reply.send({ status: 'ok' })
+        })
+      },
+    )
+
+    app.register(mercurius, {
+      schema,
+      resolvers,
+      subscription: {
+        emitter: options.emitter,
+      },
+    })
+  }
--- poc/SubscriptionProxy.ts
+++ poc/SubscriptionProxy.ts
@@ -1,82 +1,18 @@
 import Fastify from 'fastify'
 import redis from 'mqemitter-redis'
-import mercurius, { IResolvers } from 'mercurius'
-import { print } from 'graphql'
-import { Static, Type } from '@sinclair/typebox'
-import got from 'got'
 
-const gql = String.raw
+import { MercuriusSubscriptionProxyPlugin } from '././MercuriusSubscriptionProxyPlugin'
 
 const emitter = redis({
   port: 16379,
   host: '127.0.0.1',
 })
 
-const app = Fastify({ logger: true })
+const app = Fastify({ logger: true, trustProxy: true })
 
-const schema = gql`
-  type Query {
-    ok: String
-  }
-  type User {
-    id: ID!
-    name: String!
-  }
-  type Subscription {
-    user: User!
-  }
-`
-
-const resolvers: IResolvers = {
-  Subscription: {
-    user: {
-      subscribe: async (...params) => {
-        const [, , { pubsub }] = params
-        process.nextTick(() => {
-          pubsub.publish({ topic: `user:1`, payload: {} })
-        })
-        return pubsub.subscribe(`user:1`)
-      },
-      async resolve(...params) {
-        const [, , , info] = params
-        const query = print(info.operation).replace('subscription', 'query')
-        const { data } = await got
-          .post('http://127.0.0.1:1988/graphql', {
-            json: {
-              query,
-            },
-          })
-          .json()
-        return data.user
-      },
-    },
-  },
-}
-
-const invalidateSchema = Type.Object({
-  topic: Type.String(),
-})
-
-app.post<{ Body: Static<typeof invalidateSchema> }>(
-  '/v1/publish',
-  { schema: { body: invalidateSchema } },
-  (request, reply) => {
-    const { topic } = request.body
-    emitter.emit({ topic }, (err) => {
-      if (err) {
-        reply.status(500).send(err)
-      }
-      reply.send({ status: 'ok' })
-    })
-  },
-)
-
-app.register(mercurius, {
-  schema,
-  resolvers,
-  subscription: {
-    emitter,
-  },
+app.register(MercuriusSubscriptionProxyPlugin, {
+  emitter,
+  upstreamUrl: 'http://127.0.0.1:1988/graphql',
 })
 
 if (require.main === module) {
